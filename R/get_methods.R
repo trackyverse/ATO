d1f29b7993415551d17280b710bb548c0dc711ee
hugomflavio <hmflavio92@gmail.com>
2025-03-18 21:34:33 -0400

    fix get methods

--- R/get_methods.R
+++ R/get_methods.R
@@ -1,8 +1,8 @@
-setGeneric("get_detections", function(x) standardGeneric("detections"))
+setGeneric("get_detections", function(x) standardGeneric("get_detections"))
 setMethod("get_detections", "ATO", function(x) x@detections)
 
-setGeneric("get_deployments", function(x) standardGeneric("deployments"))
+setGeneric("get_deployments", function(x) standardGeneric("get_deployments"))
 setMethod("get_deployments", "ATO", function(x) x@deployments)
 
-setGeneric("get_tags", function(x) standardGeneric("tags"))
+setGeneric("get_tags", function(x) standardGeneric("get_tags"))
 setMethod("get_tags", "ATO", function(x) x@tags)
