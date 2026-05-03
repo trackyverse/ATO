7aa5db71a9a4d5880dd989334beb5f089024bb7c
hugomflavio <hmflavio92@gmail.com>
2026-05-02 21:31:36 -0300

    update has(). closes #41

--- R/has_methods.R
+++ R/has_methods.R
@@ -2,6 +2,8 @@
 #' 
 #' @param object an \code{\link{ATO}}
 #' @param value a vector with the names of the requested slots
+#' @param column a vector of column names to find within the requested slots
+#' @param allow_NA Should NAs be allowed in the inspected columns?
 #' @param error Should the code execution stop if the requested slots are empty?
 #' 
 #' @return TRUE if the slots have data, FALSE if they're empty and
@@ -15,23 +17,44 @@
 #' has(example_ato, "obs")
 #' 
 #' # alternatively, this would error
-#' # has(example_ato, "obs", TRUE)
+#' # has(example_ato, "obs", error = TRUE)
+#' 
+#' # check specific columns in slot
+#' has(example_ato, "tag", c("release_datetime", "power_level"))
+#' has(example_ato, "tag", c("test", "power_level"))
+#' # has(example_ato, "tag", c("test", "power_level"), error = TRUE)
+#' 
+#' # check that columns don't have NAs
+#' # has(example_ato, "tag",
+#' #    c("release_datetime", "power_level"),
+#' #    allow_NA = FALSE, error = TRUE)
 #' 
 #' @export
 #' 
-setGeneric("has", function(object, value, error = FALSE) standardGeneric("has"))
+setGeneric("has",
+  function(
+    object,
+    value,
+    column,
+    allow_NA = TRUE,
+    error = FALSE)
+  standardGeneric("has"))
 
 #' @rdname has
-setMethod("has", c(object = "ATO"), function(object, value, error = FALSE) {
+setMethod("has", c(object = "ATO"),
+  function(
+    object,
+    value,
+    column,
+    allow_NA = TRUE,
+    error = FALSE) {
   is_ato(object) # not necessary, the method won't let it pass if it is not ATO.
+  
   link <- !(value %in% c("det", "dep", "tag", "ani", "obs", "log"))
   if (any(link)) {
     aux <- sum(link)
-    stop(paste0(value[link], collapse = "' "),
-         ifelse(aux > 1,
-                " are not recognized slot names",
-                " is not a recognized slot name"),
-         " for an ATO.", call. = FALSE)
+    stop(.comma(value[link]), " do", .es(aux),
+         " not match ATO slot names.", call. = FALSE)
   }
   check <- sapply(value, function(i) {
     nrow(slot(object, i)) == 0
@@ -39,14 +62,56 @@ setMethod("has", c(object = "ATO"), function(object, value, error = FALSE) {
   if (any(check)) {
     aux <- sum(check)
     if (error) {
-      stop("The following ",
-           ifelse(aux > 1,
-                  " slots are empty: ",
-                  " slot is empty: "),
-           paste0(value[check], collapse = "' "),
+      stop("The following slot",
+           .s(aux),
+           " ",
+           .is(aux),
+           " empty: ",
+           .comma(value[check]),
            call. = FALSE)
     } else {
       return(FALSE)
+    }
+  }
+  if (!missing(column)) {
+    for (i in value) {
+      check <- !(column %in% colnames(slot(object, i)))
+      if (any(check)) {
+        if (error) {
+          stop("Slot ", i,
+               " does not contain column",
+               .s(sum(check)),
+               " ",
+               .comma(column[check]),
+               ".",
+               call. = FALSE)
+        } else {
+          return(FALSE)
+        }
+      }
+      if (!allow_NA) {
+        if (table_type(object) == "data.table") {
+          check <- apply(slot(object, i)[, column, with = FALSE],
+                         2, function(i) any(is.na(i)))
+        } else {
+          check <- apply(slot(object, i)[, column],
+                         2, function(i) any(is.na(i)))
+        }
+        if (any(check)) {
+          if (error) {
+            stop("NAs found in column",
+                 .s(sum(check)),
+                 " ",
+                 .comma(names(check)[check]),
+                 " of slot ",
+                 i,
+                 ".",
+                 call. = FALSE)
+          } else {
+            return(FALSE)
+          }
+        }
+      }
     }
   }
   return(TRUE)
